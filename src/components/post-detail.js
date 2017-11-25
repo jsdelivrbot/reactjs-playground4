f8b078a283bb9bfdff5b5760dbf41b7ea783969b
Damian <warnaa@gmail.com>
2017-11-25 16:20:54 +0100

    Deleting posts

--- src/components/post-detail.js
+++ src/components/post-detail.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostDetail extends Component {
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.fetchPost(id);
+    }
+
+    onDeleteButtonClick() {
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -19,7 +26,8 @@ class PostDetail extends Component {
 
         return (
             <div className="post-detail">
-                <Link to="/">Go Back</Link>
+                <Link className="btn btn-secondary" to="/">Go Back</Link>
+                <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteButtonClick.bind(this)}>Delete</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -32,4 +40,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostDetail);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostDetail);
